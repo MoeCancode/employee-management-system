c344f1951e1439aad1b69c2609ab5095f933474e
M A Razvi <abuzarflw@gmail.com>
2022-05-01 22:38:26 -0700

    Create new database and tables for department, role and employee

---
+++ database/schema.sql
@@ -0,0 +1,37 @@
+DROP DATABASE IF EXISTS corporation;
+CREATE DATABASE corporation;
+
+USE corporation;
+
+-- Create departments table
+CREATE TABLE departments (
+    id INT AUTO_INCREMENT,
+    name VARCHAR(30),
+    
+    PRIMARY KEY(id) 
+);
+
+-- Create roles table
+CREATE TABLE roles (
+    id INT AUTO_INCREMENT,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INT,
+
+    PRIMARY KEY(id),
+    FOREIGN KEY (department_id) REFERENCES departments(id)
+    ON DELETE SET NULL 
+);
+
+-- Cretae employees table
+CREATE TABLE employees (
+    id INT AUTO_INCREMENT,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT
+
+    PRIMARY KEY(id),
+    FOREIGN KEY (role_id) REFERENCES roles(id)
+    ON DELETE SET NULL 
+);
