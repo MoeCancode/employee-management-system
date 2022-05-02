ebfe3c1c7e01570708a43a028999d91a9f12c66b
M A Razvi <abuzarflw@gmail.com>
2022-05-01 23:18:47 -0700

    Add seed values to departments and roles

---
+++ database/seed.sql
@@ -0,0 +1,19 @@
+INSERT INTO departments (name) 
+VALUES 
+    ("Accounting"),
+    ("Customer Support"),
+    ("Sales"),
+    ("Human Resources"),
+    ("Management");
+
+
+INSERT INTO roles (title, salary, department_id) 
+VALUES 
+    ("Senior SalesPerson", 120000, 3),
+    ("Junior SalesPerson", 90000, 3),
+    ("Tax Accountant", 85000, 1),
+    ("Financial Analyst", 110000, 1),
+    ("Call agent", 60000, 2),
+    ("Recruiter", 75000, 1).
+    ("Human Resource representative", 70000, 4),
+    ("Regional Manager", 135000, 5);
